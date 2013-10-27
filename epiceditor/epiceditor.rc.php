5c46f005aad8634e90bcc4f1a6356a239b160659
Twiebie <jasper@digital-balance.net>
2013-10-27 02:49:55 +0100

    Trim space.

--- epiceditor/epiceditor.rc.php
+++ epiceditor/epiceditor.rc.php
@@ -6,5 +6,5 @@ Hooks=rc
 ==================== */
 
 $R['input_textarea_editor']     = '<div id="epiceditor"></div><textarea id="epic" class="editor" name="{$name}" rows="{$rows}" cols="{$cols}"{$attrs} style="display: none!important;">{$value}</textarea>{$error}';
-$R['input_textarea_medieditor'] =  '<div id="epiceditor"></div><textarea id="epic" class="medieditor" name="{$name}" rows="{$rows}" cols="{$cols}"{$attrs} style="display: none!important;">{$value}</textarea>{$error}';
-$R['input_textarea_minieditor'] =  '<div id="epiceditor"></div><textarea id="epic" class="minieditor" name="{$name}" rows="{$rows}" cols="{$cols}"{$attrs} style="display: none!important;">{$value}</textarea>{$error}';
+$R['input_textarea_medieditor'] = '<div id="epiceditor"></div><textarea id="epic" class="medieditor" name="{$name}" rows="{$rows}" cols="{$cols}"{$attrs} style="display: none!important;">{$value}</textarea>{$error}';
+$R['input_textarea_minieditor'] = '<div id="epiceditor"></div><textarea id="epic" class="minieditor" name="{$name}" rows="{$rows}" cols="{$cols}"{$attrs} style="display: none!important;">{$value}</textarea>{$error}';
